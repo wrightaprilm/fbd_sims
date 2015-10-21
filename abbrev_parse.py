90a4d63bfc3417136eead6bf7da5a2c73cf3e56d
wrightaprilm <wright.aprilm@gmail.com>
2015-10-21 10:35:06 -0500

    simplified functions, sort of

--- abbrev_parse.py
+++ abbrev_parse.py
@@ -26,52 +26,56 @@ def initializer():
 #Use the correct edges as the header
 	return(df)
 
-def get_files():
+def get_tree_list():
 	container = [file for file in glob.glob(sys.argv[2])]
+	treelist = dendropy.TreeList()	
 	for file in container:
-		print("processing file %s" % file)	
+		print("processing file %s" % file)
 		tree = dendropy.Tree.get(path=file, schema="nexus", extract_comment_metadata=True, rooting="default-unrooted")
+		treelist.append(tree)
+	return(treelist, container)
+	
+def proc_trees(treelist):
+	print treelist
+	df_list = []
+	for tree in treelist:
+		print('Calculating tree: %s' % tree)
 		node_hpd = [nd.annotations.findall(name='length_hpd95') for nd in tree.preorder_node_iter()]
 		node_med = [nd.annotations.findall(name='length_median') for nd in tree.preorder_node_iter()]
-	return(file, node_hpd, node_med)
-	
-def make_df(node_hpd, df):	
+		kvs = [nd.values_as_dict() for nd in node_hpd]
+		gnocchi = [kv.values() for kv in kvs]
+		max = [line[0][1] for line in gnocchi]
+		min = [line[0][0] for line in gnocchi]
+		df['min'] = pandas.Series(min, index=df.index)
+		df[['min']] = df[['min']].astype(float)
+		df['max'] = pandas.Series(max, index=df.index)
+		df[['max']] = df[['max']].astype(float)
+		df['boolcol'] = df['min'] < df['true']
+		df['boolcolmax'] = df['max'] > df['true']
+		kvs = [nd.values_as_dict() for nd in node_med]
+		gnocchi = [kv.values() for kv in kvs]
+		med = [float(line[0]) for line in gnocchi]
+		df['med'] = pandas.Series(med, index=df.index)
+		df['devcol'] = df['med'] - df['true']
+		df_list.append(df)
+	return(df_list)
 
-	kvs = [nd.values_as_dict() for nd in node_hpd]
-	gnocchi = [kv.values() for kv in kvs]
-	max = [line[0][1] for line in gnocchi]
-	min = [line[0][0] for line in gnocchi]
-	df['min'] = pandas.Series(min, index=df.index)
-	df[['min']] = df[['min']].astype(float)
-	df['max'] = pandas.Series(max, index=df.index)
-	df[['max']] = df[['max']].astype(float)
-	df['boolcol'] = df['min'] < df['true']
-	df['boolcolmax'] = df['max'] > df['true']
-	return(df, right_vecmin, right_vecmax)
-	
-def add_med(node_med, df):	
-	kvs = [nd.values_as_dict() for nd in node_med]
-	gnocchi = [kv.values() for kv in kvs]
-	med = [float(line[0]) for line in gnocchi]
-	df['med'] = pandas.Series(med, index=df.index)
-	df['devcol'] = df['med'] - df['true']
-	return(df)
-	
-def count_correct(df):
-	min_true = (df.boolcol==True).sum()
-	max_true = int((df.boolcolmax==True).sum())
-	count = int(df.boolcol.count())
-	print(max_true)
-	print(min_true)
+def count_correct(df_list, container):
+	for file in container:
+		print('Exporting %s' % file)
+		for df in df_list:
+			min_true = (df.boolcol==True).sum()
+			max_true = int((df.boolcolmax==True).sum())
+			count = int(df.boolcol.count())
+			df.to_csv("%s.csv" % file)
+		
 	return(min_true, max_true)	
 	
-def io_time(df, file):
-	df.to_csv("%s.csv" % file)
+#def io_time(df, file):
+	
 	
 if __name__ == "__main__":
 	df = initializer()
-	file, node_hpd, node_med = get_files()
-	df, right_vecmin, right_vecmax = make_df(node_hpd, df)
-	exported_data = add_med(node_med, df)
-	min, max = count_correct(df)
-	io_time(exported_data, file)
+	treelist, file = get_tree_list()
+	df_list = proc_trees(treelist)
+	min, max = count_correct(df_list, file)
