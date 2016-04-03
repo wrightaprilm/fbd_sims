c3fbe56c8fc08825c31073d81bee8c394665fd04
wrightaprilm <wright.aprilm@gmail.com>
2016-04-03 09:52:13 -0500

    adding multiprocess script

--- prune_script.py
+++ prune_script.py
@@ -1,17 +1,44 @@
 import dendropy
-
+import glob
+from multiprocessing import Pool
+from multiprocessing.dummy import Pool as ThreadPool 
 import sys
 
-tree = dendropy.Tree.get(path = sys.argv[1], schema = "nexus", preserve_underscores=True)
-retain_labels = []
-for node in tree.leaf_node_iter():
-	if 'T' in node.taxon.label:
-		retain_labels.append(node.taxon.label)
+def get_files():
+	flist = []
+	for file in glob.glob(sys.argv[1]):
+		flist.append(file)
+	return(flist)
 
-tree.retain_taxa_with_labels(retain_labels)
-print(tree)
-print(tree.as_ascii_plot())
+def do_prune(flist):
+	print(flist)	
 
-new_name = sys.argv[1] + '.pruned'
-tree.write(path = new_name, schema = 'nexus')
+	ts = dendropy.TreeList.get_from_path(flist, "nexus", preserve_underscores=True, tree_offset=4000)
+	retain_labels = []
+	remove_labels = []
+
+	treeZero = ts[0]
+	for node in treeZero.leaf_node_iter():
+			if 'T' in node.taxon.label:
+				retain_labels.append(node.taxon.label)
+			elif 'X' in node.taxon.label:
+				remove_labels.append(node.taxon.label)	
+
+	[tree.retain_taxa_with_labels(retain_labels) for tree in ts]
+	new_name = './' + flist + '.pruned'
+	print(new_name)
+	[ts.taxon_namespace.remove_taxon_label(label) for label in remove_labels]
+	ts.write(path = new_name, schema = 'nexus')
 		
+def calculateParallel(flist, threads=2):
+    pool = ThreadPool(threads)
+    results = pool.map(do_prune, flist)
+    pool.close()
+    pool.join()
+    return results		
+
+if __name__ == '__main__':		
+	flist = get_files()
+#	print(flist)
+	calculateParallel(flist, threads=4)
+
