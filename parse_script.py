9fd283e204e426f91019a11919ba623cc6c8d7e4
wrightaprilm <wright.aprilm@gmail.com>
2016-04-10 18:59:54 -0500

    reconfigured parsing script

--- parse_script.py
+++ parse_script.py
@@ -1,99 +1,142 @@
 # coding: utf-8
 import dendropy
 from dendropy.calculate import treemeasure
-import pandas
+import collections
+import pandas as pd
 import sys
 import collections 
 import glob
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+#Usage:
+# python comparisonTree listOfTreesSA listOfTreesNOSA outputPrefix
 
 def initializer():
 #Load Tree
-	agehpd = []
-	tree = dendropy.Tree.get(path=sys.argv[1], schema="nexus")
-#Get Edges from tree
-	print(tree)
-	tree.resolve_polytomies()
-	for node in tree.preorder_node_iter():
-			if node.annotations:
-				agehpd.append(node.annotations['age_median'].value)			 			
-			else:
-				agehpd.append('0.0')	
-#Start a pandas dataframe
-	df = pandas.DataFrame(columns=agehpd)
-#Use the correct edges as the header
-	cols = df.columns
-#And also the first row
-	return(df, cols, tree, agehpd)
+	def_tree = dendropy.Tree.get_from_path(sys.argv[1], schema='nexus')
+	satree = dendropy.Tree.get_from_path(sys.argv[2], schema='nexus')
+	fstring = sys.argv[2]
+	fst = fstring.split('.')
+	sep = '.'
+	newst = sep.join([str(fst[0]), str(fst[1]), 'nosa', str(fst[3]), str(fst[4]), str(fst[5]), str(fst[6])]) 
+	nosatree = dendropy.Tree.get_from_path(newst, schema='nexus')
+	return(def_tree, satree, nosatree)
 
-def get_files():
-	container = glob.glob(sys.argv[2])
+def get_ages(comp_tree, sa_tree, nosa_tree):	
+
 	trees = []
-	for file in container:
-		print("processing file %s" % file)
-		tree = dendropy.Tree.get(path=file, schema="nexus")		
-		trees.append(tree)
-		for tree in trees:
-			agehpd = []
-			tree.resolve_polytomies()
-	return(trees)
-	
-def get_ages(trees, dataframe, index, agedf):	
-	wd = dataframe.copy()
-	ageout = dataframe.copy()
+	trees.append(comp_tree)
+	trees.append(sa_tree)
+	trees.append(nosa_tree)
+	dub_list = []
+
 	for tree in trees:
-		agehpd = []
-		wdths = []	
-
-		for node in tree.preorder_node_iter():
-			if node.annotations:
-				agehpd.append(node.annotations['age_hpd95'].value)
-			else:
-				agehpd.append([0,0])
-		s2 = pandas.Series(agehpd, index=index)
-		dataframe = dataframe.append(s2, ignore_index=True)
-
-		agemed = []
-		for node in tree.preorder_node_iter():
-			if node.annotations:
-				agemed.append(node.annotations['age_median'].value)
-			else:
-				agemed.append(0)
-		diffs = []				
-		for age, med in zip(agemed, agedf): 
-			diffs.append(float(age) - float(med))
-		med = pandas.Series(diffs, index=index)
-		ageout = ageout.append(med, ignore_index=True)	
-
-		individ_widths = []	
-		for item in agehpd:
-			new = float(item[1]) - float(item[0])
-			individ_widths.append(new)
-		wdths = pandas.Series(individ_widths, index=index)
-		wd = wd.append(wdths, ignore_index=True)
-	return(dataframe, file, agehpd, ageout, wd)
-
-def comparisons(dataframe, hpds, index):
-	successes = []
-	for rng in hpds: 
-		min = rng[0]
-		max = rng[1]
-		for value in index:
-			if max > value > min:
-				successes.append('yes!')
-	print len(index)
-	print len(successes)
+		tree.encode_bipartitions()
+		tree.calc_node_ages(ultrametricity_precision=False)
+		tree.calc_node_root_distances()
+		bipartition_labels = collections.defaultdict(list)	
 	
-	print('Successes:', int(len(successes))/float(len(index)))
+		newdf = pd.DataFrame.from_dict(bipartition_labels, orient='index')
+		for bp in tree.bipartition_edge_map:
+			node = tree.bipartition_edge_map[bp].head_node
+			if node.annotations['age_median'].value:
+				if float(node.annotations['age_median'].value):
+					bipartition_labels[bp].append(node.annotations['age_median'].value)
+			if not node.annotations['age_median'].value:
+				if node.age > 0.0:
+					bipartition_labels[bp].append(node.age)		
+				def_df = pd.DataFrame.from_dict(bipartition_labels, orient='index')
+		dub_list.append(bipartition_labels)	
+		
+	return(def_df, dub_list)
 
-def io_time(dataframe, diff_frame, width_frame):
-	name = sys.argv[3]
-	dataframe.to_csv('%s_edges.csv' % name)
-	diff_frame.to_csv('%s_diffs.csv' % name)
-	width_frame.to_csv('%s_widths.csv' % name)
+def get_hpd(comp_tree, sa_tree, nosa_tree):	
+
+	trees = []
+	trees.append(comp_tree)
+	trees.append(sa_tree)
+	trees.append(nosa_tree)
+	hpd_list = []
+
+	for tree in trees:
+		tree.encode_bipartitions()
+		bipartition_labels = collections.defaultdict(list)	
+	
+		newdf = pd.DataFrame.from_dict(bipartition_labels, orient='index')
+		for bp in tree.bipartition_edge_map:
+			node = tree.bipartition_edge_map[bp].head_node
+			if node.annotations['age_hpd95'].value:
+				if float(node.annotations['age_hpd95'].value[0]) > 0.0 and float(node.annotations['age_hpd95'].value[1]) > 0.0:
+					bipartition_labels[bp].append(node.annotations['age_hpd95'].value)
+			if not node.annotations['age_hpd95'].value:
+				if node.age > 0.0:
+					bipartition_labels[bp].append(node.age)		
+				def_df = pd.DataFrame.from_dict(bipartition_labels, orient='index')
+		hpd_list.append(bipartition_labels)	
+	return(def_df, hpd_list)
+
+def comp_hpd(hpd_df):	
+
+	defcol = hpd_df['def'].tolist()
+	sa = hpd_df['sa'].tolist()
+	saccess = []
+	safailure = []
+	nosa = hpd_df['nosa'].tolist()
+	nosa = nosa
+	nosaccess = []
+	nosafailure = []
+	
+	for value, comp in zip(defcol, sa):
+		if float(comp[0]) < float(value) < float(comp[1]):
+			saccess.append(1)
+		else:
+			safailure.append(1)
+	print('saccess ratio:', float(len(saccess)/float(len(sa))))
+	for value, comp in zip(defcol, nosa):
+#		print(comp[0], value, comp[1])
+		if float(comp[0]) < float(value) < float(comp[1]):
+			nosaccess.append(1)
+		else:
+			nosafailure.append(1)	
+	print('nosaccess ratio:', float(len(nosaccess)/float(len(nosa))))
+		
+def io_time(def_df, list):
+	dflist = []	
+	for item in list:
+		new_df = pd.DataFrame.from_dict(item, orient='index')
+		dflist.append(new_df)	
+#		print(dflist)
+	mega_df = def_df
+#	print('dflist', len(dflist), dflist)
+	for item in dflist[1:]:
+		mega_df = pd.merge(mega_df, item, left_index=True, right_index=True)
+	mega_df.columns = ['def','sa','nosa']	
+	mega_df['nosa'] = mega_df['nosa'].astype('float64')
+	mega_df['sa'] = mega_df['sa'].astype('float64')
+	mega_df.to_csv('%s_means.csv' % sys.argv[3])
+	plt.figure()
+	mega_df.plot()
+	plt.savefig('%s_plot.png' % sys.argv[3])
+	
+def io_timehpd(def_df, list):
+	dflist = []	
+	for item in list:
+		new_df = pd.DataFrame.from_dict(item, orient='index')
+		dflist.append(new_df)	
+#		print(dflist)
+	mega_df = def_df
+#	print('dflist', len(dflist), dflist)
+	for item in dflist[1:]:
+		mega_df = pd.merge(mega_df, item, left_index=True, right_index=True)
+	mega_df.columns = ['def','sa','nosa']	
+	mega_df.to_csv('%s_hpd.csv' % sys.argv[3])
+	return(mega_df)
 
 if __name__ == "__main__":
-	dataframe, index, comp_tree, agedf = initializer()
-	trees = get_files()
-	export_data, file, hpds, diff_frame, width_frame = get_ages(trees, dataframe, index, agedf)
-	comparisons(export_data, hpds, index)
-	io_time(export_data, diff_frame, width_frame)
+	comp_tree, sa_tree, nosa_tree = initializer()
+	df, dub_list =  get_ages(comp_tree, sa_tree, nosa_tree)
+	io_time(df, dub_list)
+	df, hpd_list =  get_hpd(comp_tree, sa_tree, nosa_tree)
+	hpd_df = io_timehpd(df, hpd_list)
+	comp_hpd(hpd_df)
